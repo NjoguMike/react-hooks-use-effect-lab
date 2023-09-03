f19509e68cbfb51ffd0753690454e76ce4b46bba
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-09-04 00:17:42 +0300

    Complete UseEffect Lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(
+    ()=>{
+      const timer = setTimeout(()=>{timeRemaining === 0 ? setTimeRemaining(10) : setTimeRemaining(timeRemaining - 1)}, 1000);
+      onAnswered(false);
+      return ()=> clearTimeout(timer)
+    } ,
+    [timeRemaining]
+  )
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -13,7 +21,7 @@ function Question({ question, onAnswered }) {
   const { id, prompt, answers, correctIndex } = question;
 
   return (
-    <>
+    <div>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
@@ -25,7 +33,7 @@ function Question({ question, onAnswered }) {
         );
       })}
       <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    </div>
   );
 }
 
